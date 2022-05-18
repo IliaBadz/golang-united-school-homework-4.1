a2f0d45198c02791745341de9427e1cb25924796
IliaBadz <badzaghua.ilia@gmail.com>
2022-05-19 02:28:38 +0400

    fix: cleaner solution

--- reverse_string.go
+++ reverse_string.go
@@ -1,23 +1,9 @@
 package reverse_string
 
-import "fmt"
-
 func ReverseString(input string) (output string) {
-	newStr := []rune(input)
-	fmt.Println(newStr)
-	index := len(newStr) - 1
-
-	for i, runeValue := range input {
-		newStr[index] = runeValue
-		fmt.Println(input[i], runeValue)
-		index -= 1
+	runes := []rune(input)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
 	}
-
-	return string(newStr)
+	return string(runes)
 }
-
-// runes := []rune(s)
-//     for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-//         runes[i], runes[j] = runes[j], runes[i]
-//     }
-//     return string(runes)
